ae903767f49f47ee8afc3db020d4d85c232c1980
Anwesh43 <anweshthecool0@gmail.com>
2019-03-03 14:22:59 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedtritovaseview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtritovaseview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedtritovaseview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.tritovaseview.TriToVaseView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        TriToVaseView.create(this)
     }
 }
