f2d02bb0a6f6e12835fce606ca1b65ead21c1250
Anwesh43 <anweshthecool0@gmail.com>
2019-03-03 10:55:05 +0530

    adding libraries and constants

--- tritovaseview/src/main/java/com/anwesh/uiprojects/tritovaseview/TriToVaseView.kt
+++ tritovaseview/src/main/java/com/anwesh/uiprojects/tritovaseview/TriToVaseView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.tritovaseview
  * Created by anweshmishra on 03/03/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#4527A0")
+val backColor : Int = Color.parseColor("#212121")
